9a80ff9288a4fadac912497d3b04aed56c8d8c20
Joseph Holland <8904961+joseph-holland@users.noreply.github.com>
2025-07-06 14:35:45 +0100

    fix pwa rotation

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig({
         theme_color: '#000000',
         background_color: '#000000',
         display: 'standalone',
-        orientation: 'portrait-primary',
+        orientation: 'any',
         scope: '/',
         start_url: '/',
         icons: [
